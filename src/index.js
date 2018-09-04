3b913a56fe9db21684c7fd85f1295e3b04a37295
Stanley Makazhe <devt3000@gmail.com>
2018-09-04 13:02:27 +0200

    Added the App component to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <h1>Hello World</h1>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
