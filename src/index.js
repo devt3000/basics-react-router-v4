a524abdc7247a2c948590a7fcece6e5ca81547da
Stanley Makazhe <devt3000@gmail.com>
2018-09-04 15:47:10 +0200

    Before using "route parameters"

--- src/index.js
+++ src/index.js
@@ -1,16 +1,87 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./scss/styles.scss";
-import "./scss/styles.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './scss/styles.scss';
+import './scss/styles.css';
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Route,
+  Switch
+} from 'react-router-dom';
 
 class App extends React.Component {
-  constructor() {
-    super();
+  constructor () {
+    super()
   }
 
-  render() {
-    return <h1>Hello World</h1>;
+  render () {
+    return (
+      <Router>
+        <div>
+          <NavLinks />
+
+          <div className="views">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/admin" component={Admin} />
+              <Route path="login" component={Login} />
+              <Route render={() => <h1>404 Error</h1>} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    )
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("app"));
+function NavLinks () {
+  return (
+    <div className="links">
+      <NavLink exact to="/" className="link" activeClassName="active">
+        Home
+      </NavLink>
+      <NavLink to="/about" className="link">
+        About
+      </NavLink>
+      <NavLink to="/contact" className="link">
+        Contact Us
+      </NavLink>
+      <NavLink to="/admin" className="link">
+        Admin
+      </NavLink>
+    </div>
+  )
+}
+
+const Home = () => <h1>Home Component</h1>
+const About = () => <h1>About Component</h1>
+
+const Contact = props => (
+  <div>
+    <h1>Contact Component</h1>
+
+    <div className="links">
+      <NavLink to={`${props.match.url}/india`} className="link">
+        India Office
+      </NavLink>
+      <NavLink to={`${props.match.url}/us`} className="link">
+        Us Office
+      </NavLink>
+    </div>
+    <Switch>
+      <Route path={`${props.match.url}/india`} component={ContactIndia} />
+      <Route path={`${props.match.url}/us`} component={ContactUs} />
+      <Route render={() => <h2>No office found.</h2>} />
+    </Switch>
+  </div>
+)
+
+const ContactIndia = () => <h1>Contact India</h1>
+const ContactUs = () => <h1>Contact Us</h1>
+
+const Admin = () => <h1>Admin Component</h1>
+const Login = () => <h1>Login Component</h1>
+
+ReactDOM.render(<App />, document.getElementById('app'))
